db000437085247c11a1c22ebfb329bcb7d4da350
Alex Knowles <git@alexknowles.info>
2018-12-16 14:02:43 +0000

    Added input for begin and end tile selection

---
+++ Assets/Scripts/Components/Inputs/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using DanielEverland.ScriptableObjectArchitecture.Events.GameEvents;
+using UnityEngine;
+
+namespace MineColony.Game.Components.Inputs
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        [Range(1,4)]
+        public int PlayerNumber = 1;
+
+        public string AxisPrefix { get; internal set; }
+
+        public void OnEnable()
+        {
+            AxisPrefix = string.Format("Player{0}_", PlayerNumber);
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Components/Inputs/TileSelectionInput.cs
@@ -0,0 +1,46 @@
+﻿using DanielEverland.ScriptableObjectArchitecture.Events.GameEvents;
+using UnityEngine;
+
+namespace MineColony.Game.Components.Inputs
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class TileSelectionInput : MonoBehaviour
+    {
+        public string TileSelectionAxis = "TileSelection";
+
+        public GameEvent OnBeginTileSelection;
+        public GameEvent OnEndTileSelection;
+
+        private string _fullTileSelectionAxis;
+        private bool _selectingTiles = false;
+
+
+        public void OnEnable()
+        {
+            PlayerInput playerInput = GetComponent<PlayerInput>();
+
+            _fullTileSelectionAxis = (playerInput.AxisPrefix + TileSelectionAxis);
+        }
+
+        private void Update()
+        {
+            CheckTileSelectionInput();
+        }
+
+        private void CheckTileSelectionInput()
+        {
+            float tileSelectionAxis = Input.GetAxis(_fullTileSelectionAxis);
+
+            if (tileSelectionAxis > 0 && !_selectingTiles)
+            {
+                _selectingTiles = true;
+                OnBeginTileSelection.Raise();
+            }
+            else if (tileSelectionAxis == 0 && _selectingTiles)
+            {
+                _selectingTiles = false;
+                OnEndTileSelection.Raise();
+            }
+        }
+    }
+}
