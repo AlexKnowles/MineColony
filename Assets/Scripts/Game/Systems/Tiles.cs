1659fc1b66330c4633b83df4db0d8efe5d99930b
Alex Knowles <git@alexknowles.info>
2018-12-30 17:12:50 +0000

    Updated Tiles class to allow a distance between tiles to be set

--- Assets/Scripts/Game/Systems/Tiles.cs
+++ Assets/Scripts/Game/Systems/Tiles.cs
@@ -1,20 +1,31 @@
-﻿using DanielEverland.ScriptableObjectArchitecture.Events.GameEvents;
-using MineColony.Game.ScriptableObjectArchitecture.Collections;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace MineColony.Game.Systems
 {
     [CreateAssetMenu(menuName = "Systems/Tiles")]
     public class Tiles : ScriptableObject
     {
+        public Vector3 DistanceBetweenTiles = new Vector3(1f, 1f, 1f);
+
         public Vector3 GetTileUnderPointer(Vector3 point)
         {
-            float flooredX = Mathf.Floor(point.x);
-            float flooredY = Mathf.Floor(point.y);
-            float flooredZ = Mathf.Floor(point.z);
-            
-            return new Vector3(flooredX, flooredY, flooredZ);
+            float x = GetValueBasedOnDistance(point.x, DistanceBetweenTiles.x);
+            float y = GetValueBasedOnDistance(point.y, DistanceBetweenTiles.y);
+            float z = GetValueBasedOnDistance(point.z, DistanceBetweenTiles.z);
+
+            return new Vector3(x, y, z);
+        }
+
+        public float GetValueBasedOnDistance(float value, float distance)
+        {
+            float closestTile = (value - (value % distance));
+
+            if (value < 0)
+            {
+                closestTile -= distance;
+            }
+
+            return closestTile;
         }
     }
 }
---
+++ Assets/Scripts/Tests/Systems/Tiles/Tiles_GetTileUnderPointer.cs
@@ -0,0 +1,52 @@
+﻿using MineColony.Game.Systems;
+using MineColony.TestUtilities.Builders;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace MineColony.Tests.Systems
+{
+    public class Tiles_GetTileUnderPointer
+    {
+        [Test]
+        public void OneByOneByOne_Positive()
+        {
+            Tiles tiles = new TilesBuilder().Build();
+
+            Vector3 result = tiles.GetTileUnderPointer(new Vector3(2.3434f, 3.723123f, 9.89038f));
+
+            Assert.AreEqual(new Vector3(2f, 3f, 9f), result);
+        }
+
+        [Test]
+        public void OneByOneByOne_Negative()
+        {
+            Tiles tiles = new TilesBuilder().Build();
+
+            Vector3 result = tiles.GetTileUnderPointer(new Vector3(-93.34224f, -12.72323f, -3.12038f));
+
+            Assert.AreEqual(new Vector3(-94f, -13f, -4f), result);
+        }
+
+        [Test]
+        public void HalfByHalfByHalf_Positive()
+        {
+            Tiles tiles = new TilesBuilder().AddDistanceBetweenTiles(new Vector3(0.5f, 0.5f, 0.5f))
+                                            .Build();
+
+            Vector3 result = tiles.GetTileUnderPointer(new Vector3(6.434f, 63.8213f, 9.89038f));
+
+            Assert.AreEqual(new Vector3(6f, 63.5f, 9.5f), result);
+        }
+
+        [Test]
+        public void HalfByHalfByHalf_Negative()
+        {
+            Tiles tiles = new TilesBuilder().AddDistanceBetweenTiles(new Vector3(0.5f, 0.5f, 0.5f))
+                                            .Build();
+
+            Vector3 result = tiles.GetTileUnderPointer(new Vector3(-42.213f, -11.9464f, -88.1234f));
+
+            Assert.AreEqual(new Vector3(-42.5f, -12f, -88.5f), result);
+        }
+    }
+}
