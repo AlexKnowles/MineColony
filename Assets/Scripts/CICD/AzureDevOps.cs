abdca3d9397c8e05a0642099578f45ac63c4c511
Alex Knowles <git@alexknowles.info>
2019-02-10 08:22:01 +0000

    Addition of script to build the project via command line

---
+++ Assets/Scripts/CICD/AzureDevOps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEditor;
+
+namespace MineColony.CICD
+{
+    public class AzureDevOps
+    {
+        private static readonly string[] _scenes =
+        {
+            "Assets/Scenes/SampleScene.unity"
+        };
+
+        [MenuItem("CICD/Build Game")]
+        public static void Build()
+        {
+            string buildOutputDirectory = GetCommandLineArgumentValue("BuildOutputDirectory");
+
+            BuildPipeline.BuildPlayer(new BuildPlayerOptions
+            {
+                scenes = _scenes,
+                target = EditorUserBuildSettings.activeBuildTarget,
+                locationPathName = buildOutputDirectory,
+                targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup,
+            });
+        }
+
+        private static string GetCommandLineArgumentValue(string argumentName)
+        {
+            string[] allArguments = Environment.GetCommandLineArgs();
+
+            if (!argumentName.StartsWith("--"))
+            {
+                argumentName = ("--" + argumentName);
+            }
+
+            for (int i = 0; i < allArguments.Length; i++)
+            {
+                if (allArguments[i] == argumentName)
+                {
+                    return allArguments[i + 1];
+                }
+            }
+
+            throw new Exception("The argument '" + argumentName + "' could not be found");
+        }
+    }
+}
